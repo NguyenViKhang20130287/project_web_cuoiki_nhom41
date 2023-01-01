c6eab4cde12584df09d1e98690fa67e3d6944c3d
Nguyễn Phi Khanh <20130288@st.hcmuaf.edu.vn>
2023-01-01 19:31:10 +0700

    delete item and delete all using ajax

--- src/main/webapp/js/cart.js
+++ src/main/webapp/js/cart.js
@@ -38,14 +38,77 @@ function quantity(id, idProduct) {
 
 function DeleteItem(idProduct) {
     var cartQuantity = document.getElementById("cartQuantity");
-    console.log(idProduct);
+    var browserWidth = window.innerWidth;
+    var divId = "";
+    if (browserWidth > 428) {
+        divId = "productContainer";
+    } else {
+        divId = "productContainer2";
+    }
+    var cartTable = document.getElementById(divId);
+
     $.ajax({
         url: "/web_nhom41_war/DeleteProductControl",
         type: "post",
         data: {
-            inputId: idProduct
+            productId: idProduct,
+            browserWidth: browserWidth
         },
         success: function (data) {
+            cartTable.innerHTML = data;
+            $.ajax({
+                url: "/web_nhom41_war/CartQuantityControl",
+                type: "POST",
+                success: function (data) {
+                    var total = document.getElementById("CartTotalsID");
+                    total.innerHTML = data;
+                },
+                error: function (xhr) {
+                    //Do Something to handle error
+                }
+            });
+            $.ajax({
+                url: "/web_nhom41_war/DeleteProductControl",
+                type: "get",
+                data: {},
+                success: function (data) {
+                    cartQuantity.innerHTML = data;
+                },
+                error: function (xhr) {
+                    //Do Something to handle error
+                }
+            });
+        },
+        error: function (xhr) {
+            //Do Something to handle error
+        }
+    });
+}
+
+function DeleteAllItem() {
+    var cartQuantity = document.getElementById("cartQuantity");
+    var cartTable1 = document.getElementById("productContainer");
+    var cartTable2 = document.getElementById("productContainer2");
+
+    $.ajax({
+        url: "/web_nhom41_war/DeleteAllItemCartControl",
+        type: "post",
+        success: function (data) {
+            cartTable1.innerHTML = data;
+            cartTable2.innerHTML = data;
+            cartQuantity.innerHTML = "<a href=\"cart.jsp\"><i class=\"fa-solid fa-bag-shopping\"></i>Giỏ hàng(0)</a>";
+            $.ajax({
+                url: "/web_nhom41_war/DeleteAllItemCartControl",
+                type: "get",
+                success: function (data) {
+                    var total = document.getElementById("CartTotalsID");
+                    total.innerHTML = data;
+
+                },
+                error: function (xhr) {
+                    //Do Something to handle error
+                }
+            });
 
         },
         error: function (xhr) {
