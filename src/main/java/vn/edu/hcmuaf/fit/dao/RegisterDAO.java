bf0abe3474f4a2dba2d479616584f8d4da4ee1a0
Nguyen Vi Khang <20130287@st.hcmuaf.edu.vn>
2023-01-02 08:17:50 +0700

    fix register

--- src/main/java/vn/edu/hcmuaf/fit/dao/RegisterDAO.java
+++ src/main/java/vn/edu/hcmuaf/fit/dao/RegisterDAO.java
@@ -3,9 +3,11 @@ package vn.edu.hcmuaf.fit.dao;
 import vn.edu.hcmuaf.fit.db.DBConnect;
 import vn.edu.hcmuaf.fit.entity.Account;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
 
 public class RegisterDAO {
 
@@ -34,13 +36,17 @@ public class RegisterDAO {
     }
 
     public void signUp(String username, String email, String password) {
-        String query = "INSERT INTO `user` (`user`.username, `user`.email, `user`.`password`) VALUES(?, ?, ?)";
+        String query = "INSERT INTO `user` (`user`.username, `user`.email, " +
+                "`user`.`password`, `user`.role_id, `user`.created_at, `user`.updated_at) VALUES(?, ?, ?, ?, ?, ?)";
 
         try {
             ps = new DBConnect().getConnection().prepareStatement(query);
             ps.setString(1, username);
             ps.setString(2, email);
             ps.setString(3, password);
+            ps.setInt(4, 1);
+            ps.setDate(5, Date.valueOf(LocalDate.now()));
+            ps.setDate(6, Date.valueOf(LocalDate.now()));
             ps.executeUpdate();
         } catch (Exception e) {
 
