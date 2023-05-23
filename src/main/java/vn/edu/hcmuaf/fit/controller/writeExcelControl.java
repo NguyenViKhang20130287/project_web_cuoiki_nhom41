346fc83772b6edfc51e2e9e010c8ad942ae419d0
Nguyen Vi Khang <20130287@st.hcmuaf.edu.vn>
2023-05-23 19:57:10 +0700

    export file excel order

---
+++ src/main/java/vn/edu/hcmuaf/fit/controller/writeExcelControl.java
@@ -0,0 +1,86 @@
+package vn.edu.hcmuaf.fit.controller;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import vn.edu.hcmuaf.fit.dao.OrderDAO;
+import vn.edu.hcmuaf.fit.entity.OrderAdmin;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "writeExcelControl", value = "/admin/doc/writeExcelControl")
+public class writeExcelControl extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<OrderAdmin> list = new OrderDAO().getListOrder();
+//        PrintWriter out = response.getWriter();
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment;filename=result.xlsx");
+        XSSFWorkbook wb = new XSSFWorkbook();
+        XSSFSheet sheet = wb.createSheet("total");
+        //
+        int rowNo = 0;
+        Row row = sheet.createRow(rowNo++);
+        int cellNum = 0;
+
+        Cell cell = row.createCell(cellNum++);
+        cell.setCellValue("Mã đơn");
+
+        cell = row.createCell(cellNum++);
+        cell.setCellValue("Họ và tên");
+
+        cell = row.createCell(cellNum++);
+        cell.setCellValue("Đơn hàng");
+
+        cell = row.createCell(cellNum++);
+        cell.setCellValue("Số điện thoại");
+
+        cell = row.createCell(cellNum++);
+        cell.setCellValue("Tổng tiền");
+
+        cell = row.createCell(cellNum++);
+        cell.setCellValue("Tình trạng");
+
+        for(OrderAdmin oa: list){
+            cellNum = 0;
+            row = sheet.createRow(rowNo++);
+            cell = row.createCell(cellNum++);
+            cell.setCellValue(oa.getId());
+
+            cell = row.createCell(cellNum++);
+            cell.setCellValue(oa.getFullName());
+
+            cell = row.createCell(cellNum++);
+            cell.setCellValue(oa.getProducts().toString());
+
+            cell = row.createCell(cellNum++);
+            cell.setCellValue(oa.getPhone());
+
+            cell = row.createCell(cellNum++);
+            cell.setCellValue(oa.getTotalMoney());
+
+            cell = row.createCell(cellNum++);
+            cell.setCellValue(oa.getStatus());
+        }
+        wb.write(response.getOutputStream());
+        wb.close();
+
+        request.getRequestDispatcher("table-data-user.jsp").forward(request, response);
+//        out.println("<script type=\"text/javascript\">");
+//        out.println("alert('Xuất file thành công');");
+//        out.println("location='UserAdminControl';");
+//        out.println("</script>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
